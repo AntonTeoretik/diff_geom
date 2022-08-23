dde23eac26d1ada232c5af79c19e264dcee96af8
Anton Ablov <a.d.ablov@gmail.com>
2022-08-23 15:50:57 +0200

    Add basic n-vector

--- diff_geom/main.cpp
+++ diff_geom/main.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+#include <vec.h>
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    vec<3> A = {1,2,3};
+    vec<3> B = {1,2,3};
+    cout << (A + B * 2).nomalize().to_str() << std::endl;
+
+
+
     return 0;
 }
